e9c621c8d6126881c640584c4412e281e69fca9f
Krish Gujarati <gujaratikrish007@gmail.com>
2024-06-21 12:20:14 +0530

    Made dashboard private

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -57,10 +57,7 @@ export default function FooterCom() {
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon
-              href="https://github.com/sahandghavidel"
-              icon={BsGithub}
-            />
+            <Footer.Icon href="/" icon={BsGithub} />
           </div>
         </div>
       </div>

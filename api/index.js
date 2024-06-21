ff61a03c9ea83c3dbb8e6d486908a8fcddd71ffb
Krish Gujarati <gujaratikrish007@gmail.com>
2024-06-21 14:30:31 +0530

    update user api route

--- api/index.js
+++ api/index.js
@@ -1,32 +1,39 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from "./routes/user.route.js";
-import authRoutes from "./routes/auth.route.js";
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
-const app = express();
-app.use(express.json());
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("conn");
-}).catch( (err)=> {
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
     console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
-app.listen(3000, () =>{
-    console.log("Setvr is unning on ort 3000 !");
-});
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
-
-app.use((err,req,res,next)=>{
-    const statusCode= err.statusCode || 500;
-    const message= err.message || "Internal Server Error";
-    res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    })
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });

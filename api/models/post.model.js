08ac61ba8839f03cdde28688a97a952e49c1728d
Krish Gujarati <gujaratikrish007@gmail.com>
2024-06-22 15:15:00 +0530

    Add show more functionality to the posts results of the dashboard

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -18,7 +18,7 @@ const postSchema = new mongoose.Schema(
     image: {
       type: String,
       default:
-        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
+        'https://himalayanyoganepal.com/wp-content/uploads/2024/04/0_NAO_PgvPvUk6MN3a.jpg',
     },
     category: {
       type: String,
